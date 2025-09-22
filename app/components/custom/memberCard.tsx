901f518ea6c1ed428b2aaa1e24bb016a67b5f862
maksdeb_g <mxdave02@gmail.com>
2025-09-22 17:01:38 +0800

    fix: overflow on projects page

--- app/components/custom/memberCard.tsx
+++ app/components/custom/memberCard.tsx
@@ -12,12 +12,11 @@ export default function MemberCard({ member }: { member: Member }) {
 
   return (
     <div className="flex flex-col justify-end items-center h-[250px] w-[175px] gradient-border text-[#007983] hover:scale-105 shadow-lg transition-all duration-200">
-      <Image
+      <img
         className="h-[95%] max-w-[140%] absolute rounded-[1.5rem] pl-2 pr-2 aspect-auto"
         src={image}
         alt="Member Image"
-        
-      ></Image>
+      ></img>
       <div className="w-full flex flex-row justify-between p-3 rounded-[1.5rem] z-10 text-white gap-[10%] bg-[#007983]/50 backdrop-blur-sm">
         <h2 className="text-2xl font-semibold text-shadow-xs text-shadow-white text-start">
           {name}
